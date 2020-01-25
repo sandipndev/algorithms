de26777dea57ecd9c4c3ec3f4fb3ed0cd15e5202
Sandipan Dey <sandipandey.12@gmail.com>
2020-01-25 20:42:17 +0530

    crlf

--- graph-creation/adjacency-matrix-to-list.c
+++ graph-creation/adjacency-matrix-to-list.c
@@ -4,17 +4,14 @@
 #include <stdlib.h>
 #include "../common/list.h"
 
-int main(int argc, char **argv)
-{
-    if (argc != 2)
-    {
+int main(int argc, char **argv) {
+    if (argc != 2) {
         printf("Usage: %s [file]\n", argv[0]);
         exit(1);
     }
 
     FILE *fp = fopen(argv[1], "r");
-    if (fp == NULL)
-    {
+    if (fp == NULL) {
         printf("File not found.\n");
         exit(1);
     }
@@ -25,29 +22,26 @@ int main(int argc, char **argv)
 
     node **List = calloc(len, sizeof(node *));
     int i;
-    for (i = 0; i < len; i++)
+    for (i=0; i<len; i++)
         List[i] = NULL;
 
     i = 0;
-    while (fgets(line, LIM, fp) != NULL)
-    {
+    while (fgets(line, LIM, fp) != NULL) {
         char ch = line[0];
         int j = 0;
-        while (ch != '\0')
-        {
+        while (ch != '\0') {
             if (ch == '1')
-                List[i] = insert_last(List[i], j / 2);
+                List[i] = insert_last(List[i], j/2);
             ch = line[++j];
         }
         i++;
     }
 
-    for (i = 0; i < len; i++)
-    {
+    for (i=0; i<len; i++) {
         printf("%d -> ", i);
         print_list(List[i]);
     }
-
+    
     fclose(fp);
     return 0;
 }
